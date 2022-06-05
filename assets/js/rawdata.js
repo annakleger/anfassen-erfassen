c2c616f2ed39b25004e97a4be67cdc713d9be4eb
larissa-hoppeler <larissa.hoppeler@stud.hslu.ch>
2022-06-05 14:47:53 +0200

    animation_scorebars

--- assets/js/rawdata.js
+++ assets/js/rawdata.js
@@ -3,9 +3,9 @@ var i = 0;
 function scoretrackings() {
     if (i == 0) {
         i = 1;
-        var elem = document.getElementById("scorebar");
+        var elem = document.getElementById("scorebar1");
         var width = 1;
-        var id = setInterval(scoreborder, 100);
+        var id = setInterval(scoreborder, 36);
 
         function scoreborder() {
             if (width >= 36) {
@@ -13,10 +13,89 @@ function scoretrackings() {
                 i = 0;
             } else {
                 width++;
-                elem.style.width = width + "";
+                elem.style.width = width + "%";
                 elem.innerHTML = width + "";
             }
         }
+    }
+}
 
+function scoreaufstützen() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("scorebar2");
+        var width = 1;
+        var id = setInterval(scoreborder, 36);
+
+        function scoreborder() {
+            if (width >= 111) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+                elem.innerHTML = width + "";
+            }
+        }
+    }
+}
+
+function scorejucken() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("scorebar3");
+        var width = 1;
+        var id = setInterval(scoreborder, 36);
+
+        function scoreborder() {
+            if (width >= 101) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+                elem.innerHTML = width + "";
+            }
+        }
+    }
+}
+
+function scoregewohnheit() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("scorebar4");
+        var width = 1;
+        var id = setInterval(scoreborder, 36);
+
+        function scoreborder() {
+            if (width >= 56) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+                elem.innerHTML = width + "";
+            }
+        }
+    }
+}
+
+function scorebewusst() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("scorebar5");
+        var width = 1;
+        var id = setInterval(scoreborder, 36);
+
+        function scoreborder() {
+            if (width >= 14) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+                elem.innerHTML = width + "";
+            }
+        }
     }
 }
